d57a0f62268fdb5066c5575da9a084a9446fd60e
israa-neri <israel.aguilar@workwave.com>
2024-03-09 17:36:53 -0400

    Add Ticket model

---
+++ models/Tickets.ts
@@ -0,0 +1,11 @@
+export class Tickets {
+    id: string = '';
+    name: string = '';
+    description: string = '';
+    visibleInIncident: boolean = false;
+    visibleInRequirement: boolean = false;
+    approvalLevel: string = '';
+    status: string = '';
+
+    constructor(attributes?: any) {}
+}
